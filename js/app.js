55fbd1f78464f53ed31ce0c85eed67ec87e03ff6
andystoica <andy.stoica@me.com>
2016-04-28 11:01:25 +0100

    Fixed some CSS errors
    A few CSS errors were preventing fluid animation of several element.
    Finely tuned the responsive break points to work with most mobile
    phones.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,123 @@
 /**
+ * Gallery Items in JSON format
+ */
+
+var galleryItems = [{
+  type: "image",
+  location: "img/01.jpg",
+  thumbnail: "img/thumbnails/01.jpg",
+  title: "Hay Bales",
+  caption: "I love hay bales. Took this snap on a drive through the countryside past some straw fields."
+
+  }, {
+  type: "image",
+  location: "img/02.jpg",
+  thumbnail: "img/thumbnails/02.jpg",
+  title: "Lake",
+  caption: "The lake was so calm today. We had a great view of the snow on the mountains from here."
+
+  }, {
+  type: "image",
+  location: "img/03.jpg",
+  thumbnail: "img/thumbnails/03.jpg",
+  title: "Canyon",
+  caption: "I hiked to the top of the mountain and got this picture of the canyon and trees below."
+
+  }, {
+  type: "image",
+  location: "img/04.jpg",
+  thumbnail: "img/thumbnails/04.jpg",
+  title: "Iceberg",
+  caption: "It was amazing to see an iceberg up close, it was so cold but didn’t snow today."
+
+  }, {
+  type: "image",
+  location: "img/05.jpg",
+  thumbnail: "img/thumbnails/05.jpg",
+  title: "Desert",
+  caption: "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons."
+
+  }, {
+  type: "image",
+  location: "img/06.jpg",
+  thumbnail: "img/thumbnails/06.jpg",
+  title: "Fall",
+  caption: "Fall is coming, I love when the leaves on the trees start to change color."
+
+  }, {
+  type: "image",
+  location: "img/07.jpg",
+  thumbnail: "img/thumbnails/07.jpg",
+  title: "Plantation",
+  caption: "I drove past this plantation yesterday, everything is so green!"
+
+  }, {
+  type: "image",
+  location: "img/08.jpg",
+  thumbnail: "img/thumbnails/08.jpg",
+  title: "Dunes",
+  caption: "My summer vacation to the Oregon Coast. I love the sandy dunes!"
+
+  }, {
+  type: "image",
+  location: "img/09.jpg",
+  thumbnail: "img/thumbnails/09.jpg",
+  title: "Countryside Lane",
+  caption: "We enjoyed a quiet stroll down this countryside lane."
+
+  }, {
+  type: "image",
+  location: "img/10.jpg",
+  thumbnail: "img/thumbnails/10.jpg",
+  title: "Sunset",
+  caption: "Sunset at the coast! The sky turned a lovely shade of orange."
+
+  }, {
+  type: "image",
+  location: "img/11.jpg",
+  thumbnail: "img/thumbnails/11.jpg",
+  title: "Cave",
+  caption: "I did a tour of a cave today and the view of the landscape below was breathtaking."
+
+  }, {
+  type: "image",
+  location: "img/12.jpg",
+  thumbnail: "img/thumbnails/12.jpg",
+  title: "Bluebells",
+  caption: "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."
+
+  }, {
+  type: "video",
+  location: "https://www.youtube.com/embed/0OYt947_k-w?rel=0&amp;controls=0&amp;showinfo=0",
+  thumbnail: "img/thumbnails/13.jpg",
+  title: "Desert",
+  caption: "Some great footage of The Dam and Horseshoe Bend. It was really hot in the desert but we did a lot of flying through the canyons."
+
+  }, {
+  type: "video",
+  location: "https://www.youtube.com/embed/c8jPTY4KvSo?rel=0&amp;controls=0&amp;showinfo=0",
+  thumbnail: "img/thumbnails/14.jpg",
+  title: "Shoreline",
+  caption: "I was given a wonderful boat excursion by friends during which we discovered incredible shoreline locations"
+
+  }, {
+  type: "video",
+  location: "https://www.youtube.com/embed/q423j4UIugE?rel=0&amp;controls=0&amp;showinfo=0",
+  thumbnail: "img/thumbnails/15.jpg",
+  title: "New Zealand",
+  caption: "Aerial footage in the country side. Starts of with the bealy bridge, goes to the viaduct at aurthers pass, castle hill and lake lyndon."
+
+  }, {
+  type: "video",
+  location: "https://www.youtube.com/embed/WHnOTKUX_X0?rel=0&amp;controls=0&amp;showinfo=0",
+  thumbnail: "img/thumbnails/16.jpg",
+  title: "Aerial footage",
+  caption: "Flying the new DJI Phantom 4 over the Indian Ocean in Mauritius Island. Some more breathtaking aerial footage for all to enjoy."
+}];
+
+
+
+/**
  * Assemble gallery and zoom element
  */
 
@@ -44,8 +163,11 @@ var $zoomPrev = $('.zoom-prev');
 var $zoomNext = $('.zoom-next');
 var $zoomMedia = $('.zoom-media');
 var $zoomCaption = $('.zoom-caption');
-var $curentPosition;
-var $totalActive;
+
+// Tracking variables
+var curentPosition;
+var totalActive;
+
 
 
 /**
@@ -61,9 +183,9 @@ $('#search').keyup(function(){
       var searchTitle = $(item).children('a').children('img').attr('title');
         if (searchTitle.indexOf(searchWord) === -1)
         {
-          $(item).fadeOut(300).removeClass('active');
+          $(item).fadeOut(600).removeClass('active');
         } else {
-          $(item).fadeIn(300).addClass('active');
+          $(item).fadeIn(600).addClass('active');
         }
   });
 
@@ -130,43 +252,6 @@ function loadZoomContent(item){
 
 
 /**
-* Prev Next button navigation
-*/
-
-$zoomPrev.click(function(){
-  loadZoomContent($('.active').eq(curentPosition - 1).children('a'));
-  return false;
-});
-
-$zoomNext.click(function(){
-  loadZoomContent($('.active').eq(curentPosition + 1).children('a'));
-  return false;
-});
-
-
-
-/**
-* Keyboard navigation and event
-*/
-
-$(document).keydown(function(event){
-  if (!$zoomPrev.hasClass('zoom-button-hide') && event.which === 37)
-  {
-    loadZoomContent($('.active').eq(curentPosition - 1).children('a'));
-  }
-  else if (!$zoomNext.hasClass('zoom-button-hide') && event.which === 39)
-  {
-    loadZoomContent($('.active').eq(curentPosition + 1).children('a'));
-  }
-  else if (event.which === 27)
-  {
-    hideZoom();
-  }
-});
-
-
-
-/**
 * Activate zoom
 */
 
@@ -192,4 +277,47 @@ function hideZoom(){
 
 $zoom.click(function(){
   hideZoom();
+});
+
+
+
+/**
+* Prev Next button navigation
+*/
+
+$zoomPrev.click(function(){
+  if (!$(this).hasClass('zoom-button-hide'))
+  {
+    loadZoomContent($('.active').eq(curentPosition - 1).children('a'));
+  }
+  return false;
+});
+
+$zoomNext.click(function(){
+  if (!$(this).hasClass('zoom-button-hide'))
+  {
+    loadZoomContent($('.active').eq(curentPosition + 1).children('a'));
+  }
+  return false;
+});
+
+
+
+/**
+* Keyboard navigation and event
+*/
+
+$(document).keydown(function(event){
+  if (!$zoomPrev.hasClass('zoom-button-hide') && event.which === 37)
+  {
+    loadZoomContent($('.active').eq(curentPosition - 1).children('a'));
+  }
+  else if (!$zoomNext.hasClass('zoom-button-hide') && event.which === 39)
+  {
+    loadZoomContent($('.active').eq(curentPosition + 1).children('a'));
+  }
+  else if (event.which === 27)
+  {
+    hideZoom();
+  }
 });
